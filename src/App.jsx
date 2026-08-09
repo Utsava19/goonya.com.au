cadb01ffce97e56412a3005e38736635e0a5bd2d
Utsava19 <gurungk861@gmail.com>
2026-08-09 21:00:31 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,47 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-
+import PageTransition from "./components/PageTransition";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import OurWork from "./pages/OurWork";
 import Contact from "./pages/Contact";
-
 import "./App.css";
+
+// Scroll to top on route change
+function ScrollTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
+
+function AppInner() {
+  return (
+    <div className="site">
+      <ScrollTop />
+      <Navbar />
+      <main>
+        <PageTransition>
+          <Routes>
+            <Route path="/"         element={<Home />} />
+            <Route path="/about"    element={<About />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/our-work" element={<OurWork />} />
+            <Route path="/contact"  element={<Contact />} />
+          </Routes>
+        </PageTransition>
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="site">
-
-        <Navbar />
-
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/our-work" element={<OurWork />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-
-        <Footer />
-
-      </div>
+      <AppInner />
     </BrowserRouter>
   );
 }
