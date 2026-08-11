40180f8e8c9d018b4027bc230cb006f00b5197bb
Utsava19 <gurungk861@gmail.com>
2026-08-11 18:15:09 +1000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
+import Packages from "./pages/Packages";
 import OurWork from "./pages/OurWork";
 import Contact from "./pages/Contact";
 import "./App.css";
@@ -18,6 +19,7 @@ export default function App() {
             <Route path="/"         element={<Home />} />
             <Route path="/about"    element={<About />} />
             <Route path="/services" element={<Services />} />
+            <Route path="/packages" element={<Packages />} />
             <Route path="/our-work" element={<OurWork />} />
             <Route path="/contact"  element={<Contact />} />
           </Routes>
