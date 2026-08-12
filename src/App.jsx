2dd51c7a82c237daed6c359f663a363515756dea
Rakshya Sapkota <rakshya@Mac.nbn>
2026-08-12 14:46:09 +1000

    imp

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,32 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import ScrollToTop from "./components/ScrollToTop";
+import SEO from "./components/SEO";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Packages from "./pages/Packages";
+import OurWork from "./pages/OurWork";
+import CaseStudy from "./pages/CaseStudy";
+import Contact from "./pages/Contact";
+import Privacy from "./pages/Privacy";
+import Terms from "./pages/Terms";
+import { trackPageView } from "./utils/analytics.js";
+import "./App.css";
+import "./styles/pageLayout.css";
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPageView(`${location.pathname}${location.search}${location.hash}`);
+  }, [location.pathname, location.search, location.hash]);
+
+  return null;
+}
+
 function AppContent() {
   const location = useLocation();
 
